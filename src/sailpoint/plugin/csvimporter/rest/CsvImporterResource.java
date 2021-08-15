9b218e02c7632010b58f307c771dbec89d52d899
Krishna Vangala <currydeveloper@gmail.com>
2021-08-15 17:07:53 -0500

    Added working code.

--- src/sailpoint/plugin/csvimporter/rest/CsvImporterResource.java
+++ src/sailpoint/plugin/csvimporter/rest/CsvImporterResource.java
@@ -3,24 +3,73 @@ package sailpoint.plugin.csvimporter.rest;
 import java.util.*;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.StreamingOutput;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import sailpoint.api.SailPointContext;
+import sailpoint.api.SailPointFactory;
+import sailpoint.object.Bundle;
 import sailpoint.rest.plugin.BasePluginResource;
 import sailpoint.rest.plugin.RequiredRight;
+import sailpoint.tools.GeneralException;
 
 @RequiredRight(value = "csvImporterRESTAllow")
-@Path("csvImporter")
+@Path("csvimporter")
 public class CsvImporterResource extends BasePluginResource {
 	public static final Log log = LogFactory.getLog(CsvImporterResource.class);
+	private static final String DISABLE_ACTION="Disable";
+	private static final String ENABLE_ACTION="Enable";
 
 	@POST
 	@Path("bulkBundleUpdate")
 	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
 	@RequiredRight(value = "bulkBundleUpdate")
-	public Map postBulkBundleUpdate() {
+	public List postBulkBundleUpdate(List<Map<String,String>> roleList) throws GeneralException {
 		log.debug("POST bulkBundleUpdate");
-		Map ret = null;
-		return ret;
+		System.out.println("Got the roles as :"+roleList);
+			SailPointContext context= SailPointFactory.getCurrentContext();
+			if(null!=roleList&&!roleList.isEmpty()) {
+				String result = "Failed";
+				for (Map<String, String> recordMap : roleList) {
+					System.out.println("Record :"+recordMap);
+					String bundleName =  recordMap.get("RoleName");
+					String action = recordMap.get("Action");
+					System.out.println("Iterating for the bundle:" + bundleName);
+					Bundle bundle = context.getObjectByName(Bundle.class, bundleName);
+					System.out.println("Bundle object:" + bundle);
+					if (null != bundle) {
+						boolean isDisabled = bundle.isDisabled();
+						System.out.println("Disabled:"+isDisabled+", current action:"+action);
+						if (isDisabled && null != action && action.equalsIgnoreCase(DISABLE_ACTION)) {
+							result = "Already Disabled";
+						} else if (null != action) {
+							System.out.println("Performing action:"+action);
+							try {
+								if (action.equalsIgnoreCase(ENABLE_ACTION)) {
+									bundle.setDisabled(false);
+								} else if (action.equalsIgnoreCase(DISABLE_ACTION)) {
+									bundle.setDisabled(true);
+								}
+								context.saveObject(bundle);
+								context.commitTransaction();
+								result = "Success";
+							} catch (Exception e) {
+								System.out.println("Error occurred in bundle modification:" + e);
+								result = "Modification Failed";
+							}
+						}
+					} else {
+						System.out.println("Bundle is null:" + bundleName);
+						result = "No Matching Role found";
+					}
+					((Map) recordMap).put("Result", result);
+
+				}
+			}
+			System.out.println("Returning:"+roleList);
+		return roleList;
 	}
 
 	@Override
--- web/ui/js/app.js
+++ web/ui/js/app.js
@@ -12,11 +12,152 @@ var ksModule = angular.module('csvimporter', []);
 //});
 
 
-/**
- * Controller for the Kitchen Sink plugin.
- */
-ksModule.controller('csvimporterController', [ '$scope', function($scope) {
-	$scope.welcomeMessage="Hello, World!";
+
+ksModule.controller('csvimporterController', ["$scope", "$q", "$http", function ($scope, $q, $http) {
+	$scope.instructionsVisibility = false;
+	$scope.instructionBtnLbl = "show file instructions";
+	var CsrfToken = PluginHelper.getCsrfToken();
+	$scope.processing = undefined;
+	$scope.showInstructions = function () {
+		$scope.instructionsVisibility = $scope.instructionsVisibility ? false : true;
+		$scope.instructionBtnLbl = "hide file instructions";
+	};
+
+	$scope.downloadTemplate = function () {
+		var a = document.createElement("a");
+		var csv = "RoleName,Action\nRoleA,Enable\nRoleB,Disable"
+		if (window.navigator.msSaveOrOpenBlob) {
+			var blob = new Blob([decodeURIComponent(encodeURI(csv))], {
+				type: "text/csv;charset=utf-8;"
+			});
+			navigator.msSaveBlob(blob, 'sample.csv');
+		} else {
+
+			a.href = 'data:attachment/csv;charset=utf-8,' + encodeURI(csv);
+			a.target = '_blank';
+			a.download = 'sample.csv';
+			document.body.appendChild(a);
+			a.click();
+		}
+	};
+	var doRequest = function (method, fullPath, params, body) {
+		var def = $q.defer();
+
+		if (typeof params === 'undefined') {
+			params = "";
+		}
+
+		if (typeof body === 'undefined') {
+			body = "";
+		}
+		//$cookies.put("XSRF-TOKEN", $cookies.get("CSRF-TOKEN"));
+		$http({
+			method: method,
+			url: fullPath,
+			headers: {
+				'X-XSRF-TOKEN': CsrfToken,
+				'filter': params,
+				'Content-type': "application/json"
+			},
+			data: body
+		}).then(function (result) {
+			var d;
+			console.log(result, 'res');
+			//if the data returned is a string, parse it into a JSON object to return.
+			if (typeof result.data === 'string') {
+				console.log("data is " + result.data);
+				d = JSON.parse(result.data);
+			} else {
+				d = result.data;
+			}
+			console.log(d);
+
+			//resolves the angular promise with the data (what is sent to the 'then' function)
+			def.resolve(d);
+
+		}, function (error) {
+			def.reject("");
+		});
+
+		return def.promise;
+	};
+	$scope.submitForm = function (form) {
+		var filename = document.getElementById("bulkDirectFile");
+		if (filename.value.length < 1) {
+			($scope.warning = "Please upload a file");
+		} else {
+			var file = filename.files[0];
+			console.log(file)
+			var fileSize = 0;
+			$scope.processing = true;
+			var fileContent = [];
+			if (filename.files[0]) {
+				var reader = new FileReader();
+				reader.onload = function (e) {
+					var rows = e.target.result.split("\r\n");
+					var headers = [];
+					for (var i = 0; i < rows.length; i++) {
+						if (i == 0) {
+							headers = rows[i].split(",");
+							console.log(headers);
+						} else {
+							//Building a map of val and headers
+							var cells = rows[i].split(",");
+							var tempContent = {};
+							for (var j = 0; j < cells.length; j++) {
+								tempContent[headers[j]] = cells[j];
+							}
+							fileContent.push(tempContent);
+							console.log(fileContent);
+						}
+					}
+					console.log("final file:" + JSON.stringify(fileContent));
+					// Calling the rest Api functionality.
+					$scope.processedResults;
+					var url = PluginHelper.getPluginRestUrl('csvimporter') + '/bulkBundleUpdate';
+					console.log("url:" + url);
+					doRequest('POST', url, '', fileContent).then(function (response) {
+						$scope.processing = false;
+						$scope.resultButton = true;
+						$scope.processedResults = response;
+						console.log("Results are:" + JSON.stringify($scope.processedResults));
+						$scope.downloadResults = function () {
+							var json = $scope.processedResults;
+							var fields = Object.keys(json[0])
+							var replacer = function (key, value) { return value === null ? '' : value }
+							var csv = json.map(function (row) {
+								return fields.map(function (fieldName) {
+									return JSON.stringify(row[fieldName], replacer)
+								}).join(',')
+							})
+							csv.unshift(fields.join(',')) // add header column
+							csv = csv.join('\r\n');
+							console.log(csv);
+							// TODO : May be extract a function for later use.
+							var a = document.createElement("a");
+							if (window.navigator.msSaveOrOpenBlob) {
+								var blob = new Blob([decodeURIComponent(encodeURI(csv))], {
+									type: "text/csv;charset=utf-8;"
+								});
+								navigator.msSaveBlob(blob, 'Results.csv');
+							} else {
+
+								a.href = 'data:attachment/csv;charset=utf-8,' + encodeURI(csv);
+								a.target = '_blank';
+								a.download = 'Results.csv';
+								document.body.appendChild(a);
+								a.click();
+							}
+						}
+					});
+
+				}
+
+				reader.readAsText(filename.files[0]);
+			}
+			return false;
+		}
+	};
 }]
 
 );
